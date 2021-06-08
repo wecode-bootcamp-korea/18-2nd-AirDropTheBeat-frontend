4e8e3798bba04b7c47089df4eb9a8a64c30bc40b
ChanhoYoon <chyoon0512@gmail.com>
2021-06-08 20:02:45 +0900

    git push 에러

--- src/Components/Lists/RoomsHeader.js
+++ src/Components/Lists/RoomsHeader.js
@@ -1,16 +1,14 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import styled from 'styled-components';
 import HeaderStore from '../../store/HeaderStore';
 
 const RoomsHeader = () => {
+  const headerInfo = useContext(HeaderStore);
   return (
     <Header>
       <p>
-        <HeaderStore.Consumer>
-          {HeaderStore =>
-            `${HeaderStore.headerInfo.checkin} - ${HeaderStore.headerInfo.checkout} · 성인 ${HeaderStore.headerInfo.adult}명 · 어린이 ${HeaderStore.headerInfo.child}명 · 유아 ${HeaderStore.headerInfo.baby}명`
-          }
-        </HeaderStore.Consumer>
+        {headerInfo.headerInfo.checkin} - {headerInfo.headerInfo.checkout} · 성인 {headerInfo.headerInfo.adult}명 · 어린이
+        {headerInfo.headerInfo.child}명 · 유아 {headerInfo.headerInfo.baby}명
       </p>
       <h1>지도에서 선택한 지역의 숙소</h1>
     </Header>
@@ -19,11 +17,13 @@ const RoomsHeader = () => {
 
 const Header = styled.div`
   width: 100%;
+
   p {
     padding-bottom: 10px;
     color: #4e4e4e;
     font-size: 14px;
   }
+
   h1 {
     color: black;
     font-size: 32px;
